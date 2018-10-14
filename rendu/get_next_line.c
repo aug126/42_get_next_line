df080c23caf89fa8c2396d2d91b736e4291a1c14
Augustin Doat <adoat@w-r7-p4.s19.be>
2018-10-14 16:57:12 +0200

    gnl pret a etre rendu. push pour derniere verif

--- rendu/get_next_line.c
+++ rendu/get_next_line.c
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: adoat <marvin@42.fr>                       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/10/14 16:47:30 by adoat             #+#    #+#             */
+/*   Updated: 2018/10/14 16:53:56 by adoat            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "get_next_line.h"
 #include <stdio.h>
 
@@ -48,10 +60,8 @@ static int	copy_reste(char **line, char *reste)
 	int		up_to_char;
 	char	*temp;
 
-	if ((ret = ft_strchr(reste, '\n'))) // possible de mettre sur 1 ligne
-		up_to_char = ret - reste;
-	else
-		up_to_char = ft_strlen(reste);
+	ret = ft_strchr(reste, '\n');
+	up_to_char = ret ? (ret - reste) : ft_strlen(reste);
 	if (!(*line))
 	{
 		if (!(*line = ft_strndup(reste, up_to_char)))
@@ -74,68 +84,24 @@ static int	copy_reste(char **line, char *reste)
 
 int			get_next_line(const int fd, char **line)
 {
-	static char	reste[OPEN_MAX][BUFF_SIZE + 2]; // 2 à cause du strcpy de reste dans reste quand on veut passer la suite du char \0 trouvé à la fin.
+	static char	reste[OPEN_MAX][BUFF_SIZE + 2];
 	int			ret;
 
-	/*
-	 * Verifications
-	 */
 	if (fd < 0 || fd > OPEN_MAX || line == NULL || BUFF_SIZE <= 0)
-	{
-		printf("ERREUR du aux Vérifications !!!\n");
 		return (-1);
-	}
-	*line = NULL; // est-ce vraiment nécessaire ? line est passé comment?
-	/*
-	 * si reste vide alors on lit pour pouvoir copier
-	 * (et surtout voir si read returne 0)
-	 */
+	*line = NULL;
 	if (!(reste[fd][0]) && (ret = read(fd, reste[fd], BUFF_SIZE)) <= 0)
-	{
-		if (ret == 0)
-			printf("FIN DU FICHIER + aucun reste\n");
-		else if (ret == -1)
-			printf("ERREUR du à la PREMIERE lecture du fichier\n");
-		return (ret); // on peut retourner ret 0 ou -1 car premier ret
-	}
-	/*
-	 * copier l'ancien reste (ou le nouveau read) avant de boucler le read
-	 */
+		return (ret);
 	if ((ret = copy_reste(line, reste[fd])))
-	{
-		/* 1 si \n trouvé sinon -1 si erreur donc return value */
-		if (ret == 1)
-			printf("N trouvé au PREMIER copy\n");
-		else if (ret == -1)
-			printf("ERREUR d'allocation de mémoire au PREMIER copy\n");
-		return (ret);		
-		/* si 0 on n'a pas fini la ligne donc suite */
-	}
-	/*
-	 * tant que on ne trouve pas un \n
-	 * ou que ce n'est pas la fin du fichier, on va boucler
-	 */
+		return (ret);
 	while ((ret = read(fd, reste[fd], BUFF_SIZE)) > 0)
 	{
 		if ((ret = copy_reste(line, reste[fd])))
 		{
-			if (ret == 1)
-				printf("N trouvé dans copy BOUCLE\n");
-			if (ret == -1)
-				printf("ERREUR d'allocation de mémoire dans copy BOUCLE\n");
-			// 1 si \n trouvé sinon -1 si erreur donc return value.
-			return (ret);		
+			return (ret);
 		}
 	}
-	/*
-	 * protection du read si ret = -1
-	 * et return 1 si fin de fichier car ret = 0 mais pour le 2eme read
-	 */
 	if (ret == -1)
-	{
-		printf("ERREUR du à la lecture en BOUCLE du fichier\n");
 		return (-1);
-	}
-	printf("FIN FICHIER mais on a lu avant\n");
 	return (1);
 }
