cffb6dfc12142f1b2cb9064b3f1a527cfac373fa
aug126 <doat.augustin@gmail.com>
2018-10-14 05:08:23 +0200

    backup 2 ?

--- rendu/get_next_line.h
+++ rendu/get_next_line.h
@@ -1,15 +1,3 @@
-<<<<<<< HEAD
-#ifndef GET_NEXT_LINE_H
-# define GET_NEXT_LINE_H
-
-# include "libft.h"
-# include <stdlib.h>
-# include <unistd.h>
-
-# define BUFF_SIZE 10
-# define OPEN_MAX 50
-
-=======
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
@@ -30,6 +18,7 @@
 # include <unistd.h>
 
 # define BUFF_SIZE 10
+# define OPEN_MAX 50
 
 int		get_next_line(const int fd, char **line);
 
